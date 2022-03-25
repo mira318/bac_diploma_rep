920c3d7ffb2246dfbac89b24bc079928968e7857
mira318 <chuvilyaeva999@yandex.ru>
2022-03-25 07:51:38 +0300

    made util to check the result

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 source /home/irene/anaconda3/etc/profile.d/conda.sh
 conda activate diploma-env
-torchrun --nproc_per_node=1 --nnodes=1 --node_rank=0 ./train.py "./../data_rvl_cdip" --amp -b=16 --epochs=1 --channels-last --opt=lamb --weight-decay=0.02 --sched='cosine' --lr=5e-3 --warmup-epochs=1 --model=resnet50 --workers=4 --mixup=0.1 --cutmix=1.0 --amp --drop-path=0.05 --aa=rand-m7-mstd0.5-inc1 --reprob=0.0 --remode='pixel' --bce-loss --bce-target-thresh=0.2 --num-classes=16 --checkpoint-hist=10 --crop-pct=0.95 --seed=0 --smoothing=0.0 --train-interpolation='bicubic'
+torchrun --nproc_per_node=1 --nnodes=1 --node_rank=0 ./train.py "/DATA/ichuviliaeva/" --amp -b=16 --epochs=1 --channels-last --opt=lamb --weight-decay=0.02 --sched='cosine' --lr=5e-3 --warmup-epochs=1 --model=resnet50 --workers=8 --mixup=0.1 --cutmix=1.0 --amp --drop-path=0.05 --aa=rand-m7-mstd0.5-inc1 --reprob=0.0 --remode='pixel' --bce-loss --bce-target-thresh=0.2 --num-classes=16 --checkpoint-hist=10 --crop-pct=0.95 --seed=0 --smoothing=0.0 --train-interpolation='bicubic'
 
 
 
